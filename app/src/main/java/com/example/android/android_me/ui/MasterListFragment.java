618a194adc86a52a9a4232bcdd6eabbfcc91a896
Robert Szuba <robertadamszuba@gmail.com>
2017-06-10 19:12:51 +0200

    Create master list fragment

---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,28 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+public class MasterListFragment extends Fragment {
+
+    public MasterListFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+        final GridView gridView = (GridView) rootView.findViewById(R.id.body_grid_view);
+        final MasterListAdapter adapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+        gridView.setAdapter(adapter);
+        return gridView;
+    }
+}
